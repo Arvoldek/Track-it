e14a2a7a11b4226b3863a86b5b33d1f7d1a5c1d8
Arvoldek <arvoldek@duck.com>
2026-06-07 10:18:07 +0200

    Initial Commit

---
+++ Track it/Track_itApp.swift
@@ -0,0 +1,17 @@
+//
+//  Track_itApp.swift
+//  Track it
+//
+//  Created by Arvoldek on 07/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Track_itApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
